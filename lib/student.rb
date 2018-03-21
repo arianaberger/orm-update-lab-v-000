58df02fc4deaae9723c73a87ab0169bedd4650e2
Ariana Berger <ariana.berger@gmail.com>
2018-03-21 22:02:12 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,8 +58,8 @@ class Student
       SELECT *
       FROM students
       WHERE name = ?
+      LIMIT 1
     SQL
-    binding.pry
 
     DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
